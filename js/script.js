06436e0b4bc980e5140c82f5c68ffe02e3ecce39
IsaiStormblessed <revanisai777@gmail.com>
2021-05-13 23:03:22 -0500

    added color picker btns

--- js/script.js
+++ js/script.js
@@ -1,5 +1,8 @@
 const navToogle = document.querySelector('.nav-toogle');
 const navLinks = document.querySelectorAll('.nav__link');
+const clrPickerBtn1 = document.querySelector('#original-color');
+const clrPickerBtn2 = document.querySelector('#secondary-color');
+const root = document.querySelector(':root');
 
 
 navToogle.addEventListener('click', () => {
@@ -11,3 +14,14 @@ navLinks.forEach(link => {
 		document.body.classList.remove('nav-open');
 	});
 });
+
+
+clrPickerBtn1.addEventListener('click', () => {
+	root.style.setProperty('--clr-dark', '#303030')
+	root.style.setProperty('--clr-accent', '#16e0bd')
+})
+
+clrPickerBtn2.addEventListener('click', () => {
+	root.style.setProperty('--clr-dark', '#455D7A')
+	root.style.setProperty('--clr-accent', '#FACF5A')
+})
